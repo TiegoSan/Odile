d06b653b0ab5909224b5baffd15eb61f0c634813
TiegoSan <monkeystudio@me.com>
2026-05-14 21:05:08 +0200

    chore: Remove temporary ruby scripts

--- add_viewmodel.rb
+++
@@ -1,7 +0,0 @@
-require 'xcodeproj'
-project_path = '/Users/gautier/GogoLabs/Odile/Odile.xcodeproj'
-project = Xcodeproj::Project.open(project_path)
-target = project.targets.first
-file_ref = project.main_group.new_file('App/OdileViewModel.swift')
-target.source_build_phase.add_file_reference(file_ref)
-project.save
